575f6efcafb721bacb12d34e7c068c937c1e2558
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-13 15:38:50 +0900

    git ignore update for api key

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,8 +2,13 @@ import React from "react";
 import Movie from "./Movie";
 
 const MovieList = () => {
+  const api_key = process.env.REACT_APP_WEATHER_API_KEY;
 
-    const movies = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${}`)
+  const movies = fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query='Fight Club'`
+  );
+
+  console.log(movies);
 
   return (
     <div className="card-list">
