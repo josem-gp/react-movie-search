9a990c44fa07e051489a4da1ce56ed0016707433
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-13 20:22:40 +0900

    rendered props for each element fetched from api

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 const Movie = (props) => {
-  console.log(props.data);
+  const {
+    id,
+    original_title,
+    release_date,
+    overview,
+    popularity,
+    poster_path,
+  } = props.data;
+
   return (
-    <div className="card">
+    <div className="card" key={id}>
       <img
         className="card--image"
-        src="http://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg"
+        src={`http://image.tmdb.org/t/p/w500${poster_path}`}
+        alt="poster_img"
       ></img>
-      <h2 className="card--title">Title</h2>
-      <p>RELEASE DATE: xxxx</p>
-      <p>RATING: xx</p>
-      <p>A wealthy entrepeneur secretly creates a theme park...</p>
+      <h2 className="card--title">{original_title}</h2>
+      <p>RELEASE DATE: {release_date}</p>
+      <p>RATING: {popularity}</p>
+      <p>{overview}</p>
     </div>
   );
 };
