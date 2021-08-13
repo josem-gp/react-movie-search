da5df0d700c0e6927c0165782a6b98ffe689d21a
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-13 15:24:07 +0900

    added query to fetch api

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,6 +2,9 @@ import React from "react";
 import Movie from "./Movie";
 
 const MovieList = () => {
+
+    const movies = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${}`)
+
   return (
     <div className="card-list">
       <Movie />
